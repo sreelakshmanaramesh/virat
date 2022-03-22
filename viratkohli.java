29ccae0aece949fab13324fa11ee5da26cf6d639
sreelakshmanaramesh <sreelakshmanarameshnakka999@gmail.com>
2022-03-23 00:56:27 +0530

    adding viratkohli java

---
+++ viratkohli.java
@@ -0,0 +1,10 @@
+package com.viratkohli;
+
+public class viratkohli {
+
+	public static void main(String[] args) {
+		System.out.println("virat kohli is the best batsman in the world and he is the king of cricket");
+
+	}
+
+}
